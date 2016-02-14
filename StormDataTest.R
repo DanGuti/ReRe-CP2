f7afe968ad599d0ec0222a002336a8295e20dcc7
DanGuti <d.gutierrez.d@outlook.com>
2016-02-14 18:04:47 -0500

    Data summary for each var

--- StormDataTest.R
+++ StormDataTest.R
@@ -1,7 +1,7 @@
 # Course Project 2
 
 #Load dependencies
-library(plyr)
+require(plyr)
 
 # Retrieve zip file
 u <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
@@ -17,12 +17,11 @@ str(sData) #get some idea what variables are available.
 intvars <- c("EVTYPE","FATALITIES","INJURIES","PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP") #index of variables of interest
 sData <- sData[intvars] #reduce data to only relevant variables
 
-# Create filters for relevant data
+# Cleaning & gathering the data
 
-## Harmful events - Injuries or Fatalities
-filter1 <- (sData$FATALITIES >1 | sData$INJURIES > 1) #filter for Injury or Fatality related events.
+cData <- data.frame("EVTYPE" = sData$EVTYPE, "FATALITIES" = sData$FATALITIES, "INJURIES" = sData$INJURIES)
 
-## Economic consequences
+## Making the damage exponentials uniform numerical data
 ### Replace levels for property damage exponential
 levels(sData$PROPDMG) #look at the levels we have from property damage exponentials
 
@@ -38,7 +37,7 @@ lv <- levels(cleanPDMGEXP) #save levels of property damage exponential
 lv[1] = NA #when no damage is reported, list as NA
 levels(cleanPDMGEXP) = lv #Fix levels on cleanPDMGEXP
 cleanPDMGEXP = as.numeric(levels(cleanPDMGEXP)[cleanPDMGEXP]) #converts factor variable to numeric
-totPdmg <- sData$PROPDMG * 10^cleanPDMGEXP
+cData$totPdmg <- sData$PROPDMG * 10^cleanPDMGEXP
 
 ### Replace levels for crop damage exponential
 levels(sData$CROPDMGEXP) #look at the levels we have from property damage exponentials
@@ -53,11 +52,31 @@ lv <- levels(cleanCDMGEXP) #save levels of property damage exponential
 lv[1] = NA #when no damage is reported, list as NA
 levels(cleanCDMGEXP) = lv #Fix levels on cleanPDMGEXP
 cleanCDMGEXP = as.numeric(levels(cleanCDMGEXP)[cleanCDMGEXP])#converts factor variable to numeric, the -1 offsets the value to match the levels
-totPdmg <- sData$CROPDMG * 10^cleanCDMGEXP
+cData$totCdmg <- sData$CROPDMG * 10^cleanCDMGEXP
 
-### generate filter for complete data cases on either prop. damage or crop damage
-filter2 <- ()
+### Finally, we change NA's to 0 in the totCdmg and totPdmg variables, so that we only have dollar amounts and can come up with one single damage No.
+cData$totPdmg[is.na(cData$totPdmg)] <- 0
+cData$totCdmg[is.na(cData$totCdmg)] <- 0
+cData$totDmg <- cData$totPdmg + cData$totCdmg
 
+# Create data summaries
 
+## Harmful events - Injuries or Fatalities
+Fat <- aggregate(cData$FATALITIES, list(cData$EVTYPE), sum) #aggregate fatalitiesnam
+names(Fat) = c("EVTYPE","FATALITIES")
+FatSum <- head(arrange(Fat,desc(Fat$FATALITIES))) #retrieve 6 EVTYPEs with most fatalities
 
-## Across the United States, which types of events have the greatest economic consequences?
+Inj <- aggregate(cData$INJURIES, list(cData$EVTYPE), sum) #aggregate fatalities
+names(Inj) = c("EVTYPE","INJURIES")
+InjSum <- head(arrange(Inj,desc(Inj$INJURIES))) #retrieve 6 EVTYPEs with most fatalities
+
+## Damage
+Dam <- aggregate(cData$totDmg, list(cData$EVTYPE), sum) #aggregate fatalities
+names(Dam) = c("EVTYPE","totDmg")
+DamSum <- head(arrange(Dam,desc(Dam$totDmg))) #retrieve 6 EVTYPEs with most fatalities
+
+## Results
+op <- par(mar = c(9,4,4,2) + 0.1) ## Enlong margins for better visualization
+
+barplot(FatSum$FATALITIES, names.arg = InjSum$EVTYPE, las = 2)
+barplot(InjSum$INJURIES, names.arg = InjSum$EVTYPE, las = 2)
